d4b977f9d8736e802cd7c42479553a1bcb3519ec
Henry Philpotts <hphilpotts@gmail.com>
2023-05-04 19:41:23 +0100

    Update to all footer buttons: resets to default 'RECIPES' middle button on click.

--- src/main/Footer.js
+++ src/main/Footer.js
@@ -8,6 +8,7 @@ import Tab from '@mui/material/Tab';
 import HouseIcon from '@mui/icons-material/House';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import AddBoxIcon from '@mui/icons-material/AddBox'
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 export default function Footer({ detailView, isInDetailView }) {
 
@@ -21,21 +22,26 @@ export default function Footer({ detailView, isInDetailView }) {
     navigateTo(`${paths[newValue]}`)
   };
 
-  const backToIndex = () => {
+  const resetFooter = () => {
     isInDetailView(false)
+  }
+
+  const backToIndex = () => {
+    resetFooter()
     navigateTo('/index')
   }
+
   return (
     <footer>
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <Tabs className='footer-tabs' value={value} onChange={handleChange} aria-label="icon label tabs example">
+        <Tabs className='footer-tabs' value={value} onChange={handleChange} onClick={resetFooter} aria-label="icon label tabs example">
           <Tab to='/' icon={<HouseIcon />} label="HOME" />
           { detailView ? 
-          <Tab to='/' icon={<FastfoodIcon />} label="BACK" onClick={backToIndex}/>
+          <Tab to='/' icon={<MenuOpenIcon />} label="BACK" onClick={backToIndex}/>
           : 
           <Tab to='/' icon={<FastfoodIcon />} label="RECIPES" />
           }
-          <Tab to='/' icon={<AddBoxIcon />} label="ADD RECIPE" />
+          <Tab to='/' icon={<AddBoxIcon />} label="ADD RECIPE" onClick={resetFooter} />
         </Tabs>
       </Paper>
     </footer>
