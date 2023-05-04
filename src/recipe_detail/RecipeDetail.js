e46a3d2fa8ddc3fc942add5423ef0ef0b1f7d9a0
Henry Philpotts <hphilpotts@gmail.com>
2023-05-04 18:32:13 +0100

    Tags render in RecipeDetail component.

--- src/recipe_detail/RecipeDetail.js
+++ src/recipe_detail/RecipeDetail.js
@@ -13,33 +13,41 @@ export default function RecipeDetail() {
   const [ingredients, setIngredients] = useState([])
   const [steps, setSteps] = useState([])
 
+  const renderTags = (tags) => {
+    console.log(tags);
+    if (tags) {
+      return tags.map(tag => <span>{tag}</span>)
+    }
+  }
+
   const params = useParams()
 
-
   useEffect(() => {
-      Axios.get(`/recipes/${params.id}`)
-        .then(res => {
-          console.log(res.data)
-          setRecipe(res.data)
-          setIngredients(res.data.ingredients.map(item => item))
-          setSteps(res.data.steps)
-        })
-        .catch(err => {
-          console.error(err)
-        })
+    Axios.get(`/recipes/${params.id}`)
+      .then(res => {
+        console.log(res.data)
+        setRecipe(res.data)
+        setIngredients(res.data.ingredients.map(item => item))
+        setSteps(res.data.steps)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }, [params.id])
 
   return (
     <>
       <h1>{recipe.title}</h1>
+      {/* TODO - add image here when functionality up and running! */}
       <h2>{recipe.description}</h2>
+      <div style={{display:"flex", justifyContent:"space-evenly"}}><span style={{fontWeight:"bold"}}>Recipe tags:</span>{renderTags(recipe.tags)}</div>
       <div>
         <h3>Ingredients:</h3>
-        <RecipeIngredients ingredients={ingredients}/>
+        <RecipeIngredients ingredients={ingredients} />
       </div>
       <div>
         <h3>Steps:</h3>
-        <RecipeSteps steps={steps}/>
+        <RecipeSteps steps={steps} />
       </div>
     </>
   )
